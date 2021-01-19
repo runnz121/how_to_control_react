ba13fc05e25bed10441b2e2f4c384e3d2997a203
jbpark <runnz121@gmail.com>
2021-01-19 23:05:27 +0900

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import EventPractice from './EventPractice';
+
+
+const App = () => {
+  return <EventPractice />;
+  
+};
+
+export default App;
+
+//자바스크립트와 리엑트 비교 
+//https://velog.io/@gwak2837/React-Component%EC%99%80-Javascript-%EB%B9%84%EA%B5%90
