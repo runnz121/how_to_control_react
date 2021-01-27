e1bb0e36c02711cb6ac4950f9cdadb6a88b0f5a4
jbpark <runnz121@gmail.com>
2021-01-28 00:29:24 +0900

    20210128

--- src/App.js
+++ src/App.js
@@ -1,36 +1,23 @@
-import React, {Component} from 'react';
-import LifeCycleSample from './LifeCycleSample';
-import ErrorBoundary from './ErrorBoundary';
+import React, {useState} from 'react';
+import Info from './info'
 
-function getRandomColor() {
-  return '#' + Math.floor(Math.random()*16777215).toString(16);
-
-}
-
-class App extends Component {
-  state ={
-    color:'#000000'
-  }
-
-  handleClick = () => {
-    this.setState({
-      color:getRandomColor()
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleClick}>랜덤색상</button>
-        <ErrorBoundary>
-        <LifeCycleSample color ={this.state.color}/>
-        </ErrorBoundary>
-      </div>
-    );
-  }
-}
-
-
+const App = () => {
+  const [visible, setVisible] = useState(false);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+        >
+          {visible ? '숨기기' : '보이기'}
+        </button>
+        <hr />
+        {visible && <Info />}
+      
+    </div>
+  );
+};
 
 export default App;
 
